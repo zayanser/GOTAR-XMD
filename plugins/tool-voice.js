6976e443e36d2082a7136565af1b59d8789f380f
DybyTech <dybytech00@gmail.com>
2025-05-25 16:49:11 -0400

    Update tool-voice.js

--- plugins/tool-voice.js
+++ plugins/tool-voice.js
@@ -73,7 +73,7 @@ async (conn, mek, m, {
 
         // Send menu message with image
         const sentMsg = await conn.sendMessage(from, {  
-            image: { url: "https://files.catbox.moe/7zfdcq.jpg" },
+            image: { url: "https://files.catbox.moe/82b8gr.jpg" },
             caption: menuText
         }, { quoted: m });
 
